6550b3136d1e6d25b1ce8adedd71b807d95a083c
Andrey_Gorlov <avgorlov899@gmail.com>
2021-12-08 10:32:32 +0300

    Repository Pattern

---
+++ Mag2_DataAcces/RepositoryPattern/IRepository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mag2_DataAcces.RepositoryPattern.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Find(int id);
+
+        IEnumerable<T> GetAll( 
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>,IOrderedQueryable<T>> orderBy=null,
+            string includeProperties= null,
+            bool isTracking=true
+            );
+
+        T FristOrDefault(
+           Expression<Func<T, bool>> filter = null,
+           string includeProperties = null,
+           bool isTracking = true
+           );
+
+        void Add(T entity);
+        void Remove(T entity);
+        void Save();
+    }
+}
---
+++ Mag2_DataAcces/RepositoryPattern/Repository.cs
@@ -0,0 +1,87 @@
+﻿using Mag2_DataAcces.RepositoryPattern.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mag2_DataAcces.RepositoryPattern
+{
+    public class Repository<T> : IRepository<T> where T :class
+    {
+        private readonly ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            this.db = db;
+            this.dbSet = this.db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T FristOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))//delete пробелов
+                {
+                    query = query.Include(item);
+                }
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter!=null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties!=null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' },StringSplitOptions.RemoveEmptyEntries))//delete пробелов
+                {
+                    query = query.Include(item);
+                }
+            }
+            if (orderBy!=null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            this.db.SaveChanges();
+        }
+    }
+}
